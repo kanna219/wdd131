cf706929b5ad4fea5455ff5646fd1c55fb467c6f
kanna219 <murakamikanna05@gmail.com>
2026-05-27 15:21:08 -0600

    movies.js

--- ponder/dynamic.js
+++ ponder/dynamic.js
@@ -30,3 +30,26 @@ student.forEach(function(item){
     container.appendChild(name);
 
 })
+// literal arrray
+const pets = ['cat', 'dog', 'bird'];
+
+// constructer array
+const animals = new Array('pig', 'goat', 'sheep');
+
+pets.unshift('lizard');
+
+console.log(pets);
+
+console.log(pets[0]);
+
+pets[3] = 'bunny';
+
+console.log(pets);
+
+pets.pop();
+
+console.log(pets);
+
+pets.shift();
+
+console.log(pets);
