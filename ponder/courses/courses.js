9963ef40ca2d81a0ff7fe1fce23c17f7b2996b65
kanna219 <murakamikanna05@gmail.com>
2026-06-10 14:42:57 -0600

    corses

--- ponder/courses/courses.js
+++ ponder/courses/courses.js
@@ -2,7 +2,7 @@
 const aCourse = {
     code: 'CSE121b',
     name: 'Javascript Language',
-    logo: 'images/js-logo.png',
+    logo: 'js-logo.png',
 
     sections: [
         { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
